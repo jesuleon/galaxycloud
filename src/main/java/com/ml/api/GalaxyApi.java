e72317814d030872a3170b6e15e0f6ceb0ff5f77
jesuleon <jesuleon@gmail.com>
2017-01-18 15:00:22 -0300

    Remove unnecessary comment code

--- src/main/java/com/ml/api/GalaxyApi.java
+++ src/main/java/com/ml/api/GalaxyApi.java
@@ -7,7 +7,6 @@ import com.google.api.server.spi.config.Named;
 import com.google.cloud.datastore.*;
 import com.ml.Constants;
 import com.ml.bean.GalaxyResponse;
-import com.ml.bean.MyBean;
 import com.ml.bean.Weather;
 import com.ml.core.BuildGalaxy;
 
@@ -22,41 +21,7 @@ import com.ml.core.BuildGalaxy;
         clientIds = {Constants.WEB_CLIENT_ID})
 
 public class GalaxyApi {
-    // Create an authorized Datastore service using Application Default Credentials.
     private final Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
-
-    /**
-     * A simple endpoint method that takes a name and says Hi back
-     */
-    @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
-        MyBean response = new MyBean();
-        response.setData("Hi, " + name);
-
-
-        // Instantiates a client
-        Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
-
-        // The kind for the new entity
-        String kind = "Task";
-        // The name/ID for the new entity
-        String taskName = "sampletask1";
-        // The Cloud Datastore key for the new entity
-        Key taskKey = datastore.newKeyFactory().setKind(kind).newKey(name);
-
-        // Prepares the new entity
-        Entity task = Entity.newBuilder(taskKey)
-                .set("description", "Buy milk by " + name)
-                .build();
-
-        // Saves the entity
-        datastore.put(task);
-
-        System.out.printf("Saved %s: %s%n", task.getKey().getName(), task.getString("description"));
-
-
-        return response;
-    }
 
     @ApiMethod(httpMethod = ApiMethod.HttpMethod.GET, path = "clima", name = "clima")
     public Weather getWeatherDay(@Named("dia") int day) {
@@ -94,17 +59,4 @@ public class GalaxyApi {
 
         return response;
     }
-
-//    @ApiMethod(httpMethod = ApiMethod.HttpMethod.GET, path = "galaxy", name = "values")
-//    public GalaxyResponse getGalaxy(@Named("dia") String numberOfDays) {
-//        KeyFactory keyFactory = datastore.newKeyFactory().setKind("Galaxy");
-//        Transaction transaction = datastore.newTransaction();
-//        Entity galaxyEntity = transaction.get(keyFactory.newKey("Galaxy-" + numberOfDays));
-//
-//        GalaxyResponse response = new GalaxyResponse();
-//        response.setDroughtPeriod((int) galaxyEntity.getLong("numdroughtperiod"));
-//
-//        return response;
-//    }
-
 }
